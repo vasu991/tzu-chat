5fb258a782504a7cd7dc4b6a8b56aa5dc92457d9
Vasu Bhatia <vasubhatia991@gmail.com>
2024-06-30 21:30:44 +0530

    deploy_try_2

--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: [process.env.CLIENT_URL],
+    origin: process.env.CLIENT_URL,
 }));
 
 async function getUserDataFromRequest(req) {
